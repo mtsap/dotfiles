1b2dc5d1b50043279e4619ec0606b1d6f3604468
mtsap <mich.tsap@gmail.com>
2020-11-15 13:54:43 +0100

    moregit add --all

--- backup.sh
+++ backup.sh
@@ -1,2 +1,7 @@
 cp ~/.zshrc ./.zshrc
 cp ~/.p10k.zsh ./.p10k.zsh
+cp ~/.xinitrc ./.xinitrc
+cp ~/.config/bspwm/bspwmrc ./bspwmrc
+cp ~/.config/sxhkd/sxhkdrc ./sxhkdrc
+cp ~/.config/alacritty/alacritty.yml ./alacritty.yml
+cp ~/.config/betterlockscreenrc ./betterlockscreenrc
