6ba94de356827967eb155e694bdac72c5756cc57
mtsap <mich.tsap@gmail.com>
2020-11-15 15:39:25 +0100

    More

--- backup.sh
+++ backup.sh
@@ -5,3 +5,4 @@ cp ~/.config/bspwm/bspwmrc ./bspwmrc
 cp ~/.config/sxhkd/sxhkdrc ./sxhkdrc
 cp ~/.config/alacritty/alacritty.yml ./alacritty.yml
 cp ~/.config/betterlockscreenrc ./betterlockscreenrc
+cp -r ~/.config/nvim ./nvim
