0c552a9f680f036a27508523ab69a5815a64b766
Michael Tsapanos <michael.tsapanos@ato-gear.com>
2022-06-24 10:07:03 +0200

    Update dot_config/nvim/lua/cmp-config.lua Update dot_config/nvim/lua/lsp-config.lua

--- dot_config/nvim/lua/cmp-config.lua
+++ dot_config/nvim/lua/cmp-config.lua
@@ -1,66 +1,117 @@
 local cmp = require'cmp'
 local lspkind = require('lspkind')
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+local util = require 'lspconfig/util'
+
 require('cmp-npm').setup({})
 
-  cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
-  cmp.setup({
-    snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
-      end,
-    },
-    mapping = {
-      ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-      ['<C-e>'] = cmp.mapping({
-        i = cmp.mapping.abort(),
-        c = cmp.mapping.close(),
-      }),
-      ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's', 'c' }),
-      ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's', 'c' }),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    },
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },
-      { name = 'vsnip' }, -- For vsnip users.
-      { name = 'path'},
-      { name = 'treesitter' },
-      { name = 'nvim_lsp_signature_help' },
-      { name = 'npm', keyword_length = 3 },
-    }, {
-      { name = 'buffer' },
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+      -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+      -- require'snippy'.expand_snippet(args.body) -- For `snippy` users.
+    end,
+  },
+  mapping = {
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ['<C-e>'] = cmp.mapping({
+      i = cmp.mapping.abort(),
+      c = cmp.mapping.close(),
     }),
-    formatting = {
-      format = lspkind.cmp_format(),
-    },
-  })
+    ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's', 'c' }),
+    ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's', 'c' }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    { name = 'path'},
+    { name = 'treesitter' },
+    { name = 'nvim_lsp_signature_help' },
+    { name = 'npm', keyword_length = 3 },
+  }, {
+    { name = 'buffer' },
+  }),
+  formatting = {
+    format = lspkind.cmp_format(),
+  },
+})
 
   -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline('/', {
-    sources = {
-      { name = 'buffer' }
-    }
-  })
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
 
   -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-  cmp.setup.cmdline(':', {
-    sources = cmp.config.sources({
-      { name = 'path' }
-    }, {
-      { name = 'cmdline' }
-    })
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
   })
+})
 
-  -- Setup lspconfig.
-  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-  -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-  require('lspconfig')['tsserver'].setup {
-    capabilities = capabilities
+
+local on_attach = function(client, bufnr)
+  print("Attach function called in cmp")
+  
+local opts = { noremap=true, silent=true }
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+  -- Mappings.
+  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  buf_set_keymap('n', '<leader>dd', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+
+
+-- autoformat only for haskell
+  if vim.api.nvim_buf_get_option(0, 'filetype') == 'haskell' then
+      vim.api.nvim_command[[
+          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+  end
+
+end
+
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local lspconfig = require('lspconfig')
+
+lspconfig.eslint.setup{
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx" },
+}
+
+lspconfig.tsserver.setup {
+  capabilities = capabilities,
+  on_attach = on_attach,
+  cmd = { "typescript-language-server", "--stdio" },
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  init_options = {
+      hostInfo = "neovim"
+   },
+  root_dir = util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")
+}
+
+
+lspconfig.hls.setup{
+    on_attach = on_attach,
+    cmd = { "haskell-language-server-wrapper", "--lsp" },
+    filetypes = { "haskell", "lhaskell" },
+    root_dir = util.root_pattern("*.cabal", "stack.yaml", "cabal.project", "package.yaml", "hie.yaml"),
+    settings = {
+      haskell = {
+        hlintOn = true,
+        formattingProvider = "fourmolu"
+      }
+    },
+    single_file_support = true,
   }
--- dot_config/nvim/lua/lsp-config.lua
+++ dot_config/nvim/lua/lsp-config.lua
@@ -1,67 +1,9 @@
-local nvim_lsp = require('lspconfig')
-local util = require 'lspconfig/util'
-
-local on_attach = function(client, bufnr)
-
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+local opts = { noremap=true, silent=true }
+-- vim.keymap.set('n', 'K', vim.lsp.buf.hover,opts)
+vim.keymap.set('n', 'rr', vim.diagnostic.goto_next,opts)
+vim.keymap.set('n', 're', vim.diagnostic.goto_prev,opts)
 
 
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<leader>dd', '<Cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', '<leader>r', '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>e', '<Cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-
--- autoformat only for haskell
-  if vim.api.nvim_buf_get_option(0, 'filetype') == 'haskell' then
-      vim.api.nvim_command[[
-          autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
-  end
-
-end
-
--- Use a loop to conveniently call 'setup' on multiple servers and
--- map buffer local keybindings when the language server attaches
---local servers = { 'tsserver' }
---for _, lsp in ipairs(servers) do
-  --nvim_lsp[lsp].setup {
-    --on_attach = on_attach,
-    --flags = {
-      --debounce_text_changes = 150,
-    --}
-  --}
---end
-
-
-nvim_lsp.eslint.setup{}
-nvim_lsp.tsserver.setup {
-  on_attach = on_attach,
-  cmd = { "typescript-language-server", "--stdio" },
-  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
-  init_options = {
-      hostInfo = "neovim"
-    }
-  -- root_dir = root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")
-}
-
-
-nvim_lsp.hls.setup{
-    on_attach = on_attach,
-    cmd = { "haskell-language-server-wrapper", "--lsp" },
-    filetypes = { "haskell", "lhaskell" },
-    root_dir = util.root_pattern("*.cabal", "stack.yaml", "cabal.project", "package.yaml", "hie.yaml"),
-    settings = {
-      haskell = {
-        hlintOn = true,
-        formattingProvider = "fourmolu"
-      }
-    },
-    single_file_support = true,
-  }
 
 vim.diagnostic.config({
     underline = true,
