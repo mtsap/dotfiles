93686015923e87179973a940d90b385b2f6f2ce9
Michael <mich.tsap@gmail.com>
2024-01-05 11:35:10 +0200

    update kitty, remove nvim

--- dot_config/nvim/ftdetect/blocks.vim
+++
@@ -1,1 +0,0 @@
-au BufRead,BufNewFile *.blocks                set filetype=blocks
--- dot_config/nvim/lua/plugins/disabled.lua
+++
@@ -1,8 +0,0 @@
-return {
-  { "echasnovski/mini.surround", enabled = false },
-  { "ggandor/leap.nvim",         enabled = false },
-  { "ggandor/flit.nvim",         enabled = false },
-  { "SmiteshP/nvim-navic",       enabled = false },
-  { "folke/persistence.nvim",    enabled = false },
-  { "folke/flash.nvim",          enabled = false },
-}
